168713cb4b4c9b6f0a993119b9faa2b536a9c91c
Anupam Kumar <akanupamsingh25@gmail.com>
2024-10-10 01:38:26 +0530

    New

--- LoopQuestions/question4.java
+++
@@ -1,9 +0,0 @@
-package LoopQuestions;
-
-public class question4 {
-    public static void main(String[] args){
-        for(; ;){
-            System.out.println("Anupam");
-        }
-    }
-}
--- variables.java
+++
@@ -1,9 +0,0 @@
-public class variables{
-    public static void main(String[] args){
-        int a = 10, b = 5;
-        int ans;
-        ans = (a * b)/(a - b);
-        System.out.println(ans);
-
-    }
-}
